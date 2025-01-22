8387d6db3e8e72ea23314f1bac77b96ca19798b4
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-01-22 18:30:30 +0300

    Add files via upload
    subtraction

---
+++ saving_taxes.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int x, y;
+    cin >> x >> y;
+    int sum = x - y;
+    cout << sum << endl;
+  }
+  return 0;
+}
