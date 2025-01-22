948ab9eea42fe9ec42d67ca7b341757fc3f8d5e3
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-01-22 18:49:13 +0300

    Add files via upload

---
+++ Biryani_classes.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int x, y;
+    cin >> x >> y;
+    int multiplaction = x * y;
+    cout << multiplaction << endl;
+  }
+  return 0;
+}
