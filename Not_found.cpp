950ffb658308a1b5074b21af73c0f57cd0bfab39
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-03-27 18:24:33 +0300

    Add files via upload
    just check the value

---
+++ Not_found.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int n;
+  cin >> n;
+  if (n==404)
+  {
+    cout << "NOT FOUND" << endl;
+  }
+  else
+  {
+    cout << "FOUND" << endl;
+  }
+  return 0;
+}
