db1605bf36dedc2778574fc2b2119eb0e5c6adf6
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-03-28 08:24:50 +0300

    Add files via upload
    if else statement

---
+++ puzzle.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin >> n;
+  if (6<=n&&n<=8)
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+  return 0;
+}
