9468b53abcfb7d61d66f3cdb259886c6a0d21aa8
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-04-13 23:44:04 +0600

    Add files via upload
    just x*y*z that's it and print output

---
+++ chef_and_chapters.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int x, y, z;
+    cin >> x >> y >> z;
+    int sum = x * y * z;
+    cout << sum << endl;
+  }
+  return 0;
+}
