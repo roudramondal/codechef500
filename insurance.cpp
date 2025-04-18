2751a2d5f71be34ffe93f3cd45d6daaa5d1f1ce5
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-04-18 23:08:58 +0600

    Add files via upload
    just find the min value from x and y

---
+++ insurance.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int x, y;
+    cin>>x>>y;
+    int value_find = min(x, y);
+    cout<<value_find<<endl;
+    
+  }
+  return 0;
+}
