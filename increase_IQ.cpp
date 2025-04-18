c3aa58071aa5a5ad592285d773f8acfa0a22520e
Code__R <103403881+roudramondal@users.noreply.github.com>
2025-04-18 23:13:41 +0600

    Add files via upload
    add 7 with x  and check the condition that's it

---
+++ increase_IQ.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int x;
+  cin >> x;
+  int condition_value = x + 7;
+  if (condition_value>170)
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+  return 0;
+}
